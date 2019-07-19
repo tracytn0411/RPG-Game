956d4c16d9f0fb58ad5a52730442dbfb3aa078ba
TracyTN <tracytn0411@gmail.com>
2019-07-19 15:39:32 -0700

    Update attacker and defender profiles.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,8 @@ $(function () {
             nickName: "luke",
             name: "Luke Skywalker",
             firstImg: "assets/images/first Luke.jpg",
-            secondImg: '<img src="assets/images/second Luke.jpg">',
+            fighterImg: "assets/images/fighter Luke.jpg",
+            defenderImg: "assets/images/defender Luke.jpg",
             healthPoints: 150,
             attackPower: 18,
             counterPower: 15
@@ -13,7 +14,8 @@ $(function () {
             nickName: "rey",
             name: "Rey",
             firstImg: "assets/images/first Rey.jpg",
-            secondImg: '<img src="assets/images/second Rey.jpg">',
+            fighterImg: "assets/images/fighter Rey.jpg",
+            defenderImg: "assets/images/defender Rey.jpg",
             healthPoints: 200,
             attackPower: 14,
             counterPower: 20
@@ -22,7 +24,8 @@ $(function () {
             nickName: "ren",
             name: "Kylo Ren",
             firstImg: "assets/images/first Kylo Ren.jpg",
-            secondImg: '<img src="assets/images/second Kylo Ren.jpg">',
+            fighterImg: "assets/images/fighter Kylo Ren.jpg",
+            defenderImg: "assets/images/defender Kylo Ren.jpg",
             healthPoints: 250,
             attackPower: 12,
             counterPower: 10
@@ -31,7 +34,8 @@ $(function () {
             nickName: "snoke",
             name: "Snoke",
             firstImg: "assets/images/first Snoke.jpeg",
-            secondImg: '<img src="assets/images/second Snoke.jpg">',
+            fighterImg: "assets/images/fighter Snoke.jpg",
+            defenderImg: "assets/images/defender Snoke.jpg",
             healthPoints: 300,
             attackPower: 10,
             counterPower: 5
@@ -39,38 +43,66 @@ $(function () {
     ];
     
     var characterNo = charactersArray.length;
-
     console.log(characterNo);
-
+    var fighterID;
+    var defenderID;
+    var attackButton;
     newGame();
 
     function newGame() {
-        $("#characters").before($("<h2>").text("Please select your character"));
-            $.each(charactersArray, function (){
-                var divTest = $("<div>").addClass("col-3").attr("data_name",this.nickName);
-                $("#characters").append(divTest);
+        fighterID = "";
+        defenderID = "";
 
-                divTest.append($("<h3>").text(this.name));
-                divTest.append($("<img>").addClass("img-fluid").attr("src",this.firstImg).attr("data_name",this.name).attr("data_hp",this.healthPoints));
-                divTest.append($("<p>").text("HP:\xa0"+this.healthPoints)); //\xa0 is whitespace
+        //Create attack button and hide it
+        attackButton = $("#attack").append($("<button>").addClass("btn").attr("type","button"));
+        attackButton.hide();
+
+        //Create guidance board 
+        $("#characters_list").before($("<h2>").attr("id","characterOptions").text("Available Characters"));
+
+        //Loop thru objects to create profile for each character
+        $.each(charactersArray, function (){
+            var divCharacter = $("<div>").addClass("col-3 character").attr({
+                //store all data in attribute to use later
+                "data_name" : this.name,
+                "data_hp" : this.healthPoints,
+                "data_ap" : this.attackPower,
+                "data_cp" : this.counterPower,
+                "data_fightimg" : this.fighterImg,
+                "data_defendimg" : this.defenderImg,
             })
+            $("#characters_list").append(divCharacter);
+        
+            //Display character profiles
+            divCharacter.append($("<h3>").text(this.name));
+            divCharacter.append($("<img>").addClass("img-fluid").attr("src",this.firstImg));
+            divCharacter.append($("<p>").text("HP:\xa0"+this.healthPoints)); //\xa0 is whitespace
+        })
     }
 
-    $("img").on("click", function () {
-        if (characterNo === 4) {
-            var pickCharacter = 
-                [
+    $(".character").on("click", function () {
+        if (fighterID == "") {
+            fighterID = [
+                $("<h3>").text($(this).attr("data_name")),
+                $("<img>").addClass("img-fluid").attr("src", $(this).attr("data_fightimg")),
+                $("<h5>").text("HP:\xa0" + $(this).attr("data_hp"))
+            ];
+            $("#vsFighter").append(fighterID); //Move chosen character to fighter
+            $(this).remove(); //Remove chosen fighter old profile 
+            attackButton.show(); //Show attack button
+            $("#characterOptions").text("Available Enemies"); //Change character options tittle
+
+        } else {
+            if (defenderID == "") {
+                defenderID = [
                     $("<h3>").text($(this).attr("data_name")),
-                    $("<img>").addClass("img-fluid").attr("src", $(this).attr("src")),
+                    $("<img>").addClass("img-fluid").attr("src", $(this).attr("data_defendimg")),
                     $("<h5>").text("HP:\xa0" + $(this).attr("data_hp"))
                 ];
-            //$(this).attr("src");
-            $("#vsFighter").append(pickCharacter);
-            $
-
+                $("#vsDefender").append(defenderID);//Character becomes defender
+                $(this).remove(); //Remove defender old profile
+            }
         }
+    });
 
-    })
-
-    
 });
